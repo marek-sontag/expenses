df5ef5fb4f44ac30adbd65116324cf8b70f02bce
Marek Sontag <mareksontag@gmail.com>
2020-02-20 11:22:03 +0100

    Expenses

--- src/main/java/pl/sda/javabasics/expenses/Expense.java
+++ src/main/java/pl/sda/javabasics/expenses/Expense.java
@@ -5,6 +5,14 @@ import java.time.LocalDateTime;
 
 public class Expense {
 
+    public static final Category[] CATEGORIES = {
+            new Category(0, "Opłaty stałe"),
+            new Category(1, "Jedzenie"),
+            new Category(2, "Rozrywka"),
+            new Category(3, "Ubrania"),
+            new Category(4, "Inne")
+    };
+
     private LocalDateTime timestamp;
     private BigDecimal amount;
     private int category;
