2064a4b298d0cafeee49b0302c9d1dd040c477cf
Marek Sontag <mareksontag@gmail.com>
2020-02-20 12:34:46 +0100

    Menu skeleton

--- src/main/java/pl/sda/javabasics/expenses/Expenses.java
+++ src/main/java/pl/sda/javabasics/expenses/Expenses.java
@@ -8,11 +8,56 @@ import static pl.sda.javabasics.expenses.Expense.CATEGORIES;
 
 public class Expenses {
 
+    final static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         System.out.println("Witaj w rejestrze wydatków!");
 
+        int option = -1;
+        do {
+            printMenu();
+            String optionString = scanner.nextLine();
+            while (!optionString.matches("[0-4]")) {
+                printMenu();
+                optionString = scanner.nextLine();
+            }
+            option = Integer.parseInt(optionString);
+
+            switch (option) {
+                case 0:
+                    break;
+                case 1:
+                    addExpense();
+                    break;
+                case 2:
+                    // show expenses
+                    break;
+                case 3:
+                    // show expenses from category
+                    break;
+                case 4:
+                    // show expenses greater than
+                    break;
+                default:
+                    System.out.println("Niepoprawna opcja. Wybierz liczbę od 0-4.");
+                    break;
+            }
+        } while (option != 0);
+
+        System.out.println("Żegnaj!");
+    }
+
+    private static void printMenu() {
+        System.out.println("Co chcesz zrobić?");
+        System.out.println("1. Dodaj wydatek");
+        System.out.println("2. Wyświetl wydatki");
+        System.out.println("3. Wyświetl wydatki z wybranej kategorii");
+        System.out.println("4. Wyświetl wydatki większe niż X");
+        System.out.println("0. Zakończ program");
+    }
+
+    private static Expense addExpense() {
         System.out.println("Podaj kwotę wydatku:");
-        Scanner scanner = new Scanner(System.in);
         String amountString = scanner.nextLine();
         while (!amountString.matches("\\d+(\\.\\d{1,2})?")) {
             System.out.println("Nieprawidłowy format kwoty, wpisz ponownie!");
@@ -31,7 +76,6 @@ public class Expenses {
 
         System.out.printf("Kwota: %s, kategoria: %s\n", amountString, categoryString);
 
-        Expense expense = new Expense(LocalDateTime.now(), new BigDecimal(amountString), CATEGORIES[Integer.parseInt(categoryString) - 1]);
-
+        return new Expense(LocalDateTime.now(), new BigDecimal(amountString), CATEGORIES[Integer.parseInt(categoryString) - 1]);
     }
 }
